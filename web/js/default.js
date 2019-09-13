755b2c3121d61801bb08457f6d650b18dc372b6d
rober <rober@DESKTOP-T3629F1>
2019-09-13 05:56:50 -0600

    Creacion de consultas, citas, ventas, doctores y abonos

--- web/js/default.js
+++ web/js/default.js
@@ -1,3 +1,58 @@
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+$(document).ready(function () {
+    $('#example').DataTable({
+        //Poder seleccionar 1 Columna
+        responsive: true,
+        select: true,
+        //Configuracion del lenguaje del datatable
+        "language": {
+            sProcessing: "Procesando...",
+            sLengthMenu: "Mostrar _MENU_ Registros",
+            sZeroRecords: "No se encontraron resultados",
+            sEmptyTable: "Ningún dato disponible en esta tabla",
+            sInfo: "_START_ - _END_ ",
+            sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+            sInfoPostFix: "",
+            sSearch: "Buscar:",
+            sUrl: "",
+            sInfoThousands: ",",
+            sLoadingRecords: "Cargando...",
+            "paginate": {
+                "first": "Primera",
+                "last": "Última",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+        },
+        "lengthMenu": [5, 10, 20, 25],
+    });
+    $('#example1').DataTable({
+        //Poder seleccionar 1 Columna
+        select: true,
+        //Configuracion del lenguaje del datatable
+        "language": {
+            sProcessing: "Procesando...",
+            sLengthMenu: "Mostrar _MENU_ Registros",
+            sZeroRecords: "No se encontraron resultados",
+            sEmptyTable: "Ningún dato disponible en esta tabla",
+            sInfo: "_START_ - _END_ ",
+            sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+            sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+            sInfoPostFix: "",
+            sSearch: "Buscar:",
+            sUrl: "",
+            sInfoThousands: ",",
+            sLoadingRecords: "Cargando...",
+            "paginate": {
+                "first": "Primera",
+                "last": "Última",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+        },
+        "lengthMenu": [4],
+    });
+
+
+});
+
